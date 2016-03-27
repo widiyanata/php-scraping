aaa0e533689bf7ff2fd65984ee62f9ec128d27f8
widiyanata <widyanata48@gmail.com>
2016-03-27 16:56:31 +0700

    simple curl

--- scrap.php
+++ scrap.php
@@ -3,8 +3,18 @@
 function curlGet ($url) {
 	$ch = curl_init(); // initiation curl session
 
-	# Setting cURL option
+	# Setting cURL options
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_URL, $url);
 
+	$results = curl_exec($ch); // executing cURL session
 
+	curl_close($ch);
+
+	return $results;
 
 }
+
+$packtPage = curlGet('http://www.lazada.co.id/perangkat-permainan-pc/');
+
+echo $packtPage;
