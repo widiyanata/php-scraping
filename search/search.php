edc2d091760728a7cd8e93661cca113cf907155b
widiyanata <widyanata48@gmail.com>
2016-03-28 13:49:09 +0700

    function for scraping content between two string

--- search/search.php
+++ search/search.php
@@ -21,3 +21,36 @@ function returnXPathObject($item) {
 
 	return $xmlPageXPath;
 }
+
+function scrapeBetween($item, $start, $end) {
+
+	if (($startPos = stripos($item, $end)) === false) {
+		return false;
+	} else if (($endPos = stripos($item, $end)) == false) {
+		return false;
+	} else {
+		$substrStart = $startPos + strlen($start);
+
+		return substr($item, $substrStart, $endPos - $substrStart);
+	}
+
+}
+
+$resultsPages = array();
+$pages = array();
+
+$initialResultsPageUrl = 'http://www.packtpub.com/books?keys=php';
+
+$resultsPages[] = $initialResultsPageUrl;
+
+$initialResultsPageSrc = curlGet($initialResultsPageUrl);
+
+$resultsPageXPath = returnXPathObject($initialResultsPageSrc);
+$resultsPageUrls = $resultsPageXPath->query('//ul[@class="pager"]/li/a/@href');
+
+if ($resultsPageUrls->length > 0) {
+	for ($i = 0; $i < $resultsPageUrls->length; $i++) {
+		$resultsPages[] = 'http://www.packtpub.com' . $resultsPageUrls->item($i)->nodeValue;
+	}
+}
+
